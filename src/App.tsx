9af0ecf6c8a0691211dc3ad1adaf7cdf540a4a2d
sravan456964 <richie0987654@gmail.com>
2025-09-16 09:54:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -33,8 +33,8 @@ const App = () => (
                 <Route path="/about" element={<About />} />
                 <Route path="/features" element={<Features />} />
                 <Route path="/demo" element={<Demo />} />
-                <Route path="/docs" element={<Documentation />} />
-                <Route path="/contact" element={<Contact />} />
+                {/* <Route path="/docs" element={<Documentation />} /> */}
+               {/*  <Route path="/contact" element={<Contact />} />  */}
                 <Route path="/auth" element={<Auth />} />
                 <Route path="/logs" element={<Logs />} />
                 {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
